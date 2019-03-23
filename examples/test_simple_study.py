986f8e0167e14ee35a2b56a52a5cbb3390484af6
laihongchang <laihongchang@daixiaomi.com>
2019-03-24 01:05:35 +0800

    update study.trials_list

---
+++ examples/test_simple_study.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+examples/test_simlple_study.py was created on 2019/03/22.
+file in :relativeFile
+Author: Charles_Lai
+Email: lai.bluejay@gmail.com
+"""
+import os
+import sys
+root = os.path.dirname(os.path.abspath(__file__))
+sys.path.append("%s/../.." % root)
+sys.path.append("%s/.." % root)
+sys.path.append("%s/../diego" % root)
+sys.path.append("%s/../../.." % root)
+sys.path.append(u"{0:s}".format(root))
+from diego.study import create_study
+
+if __name__ == "__main__":
+    import sklearn.datasets
+    digits = sklearn.datasets.load_digits()
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(digits.data, digits.target,
+                                                                                train_size=0.75, test_size=0.25)
+
+    s = create_study(X_train, y_train)
+    s.generate_autosk_trial(mode='fast')
+    s.optimize(X_test, y_test)
